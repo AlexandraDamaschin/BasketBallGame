b66e8a15f953a23f795cbfdf57f1e035ada9d25b
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-12-27 17:10:50 +0000

    libraries added

--- app/src/main/java/com/example/android/basketballgame/MainActivity.java
+++ app/src/main/java/com/example/android/basketballgame/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.android.basketballgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
 }
