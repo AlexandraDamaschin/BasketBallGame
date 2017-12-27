7a8676db3fefad1a8be76c3443081129b34f67bb
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2017-12-27 17:27:47 +0000

    template finish team A

--- app/src/main/java/com/example/android/basketballgame/MainActivity.java
+++ app/src/main/java/com/example/android/basketballgame/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+    
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
