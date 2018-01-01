c862a7bdc641fa50438ed885e89b8bf0fcf9959d
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-01-01 21:05:38 +0000

    increment points team B

--- app/src/main/java/com/example/android/basketballgame/MainActivity.java
+++ app/src/main/java/com/example/android/basketballgame/MainActivity.java
@@ -8,32 +8,56 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    int team_a_score=0;
+    int team_a_score = 0;
+    int team_b_score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA (team_a_score);
+        displayForTeamA(team_a_score);
+        displayForTeamB(team_b_score);
     }
 
+    //team A score
     public void displayForTeamA(int score) {
-        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-        scoreView.setText(String.valueOf(score));
+        TextView scoreViewA = findViewById(R.id.team_a_score);
+        scoreViewA.setText(String.valueOf(score));
     }
 
-    public void increment3Points (View view){
+    public void increment3PointsA(View view) {
         team_a_score += 3;
         displayForTeamA(team_a_score);
     }
 
-    public void increment2Points (View view){
+    public void increment2PointsA(View view) {
         team_a_score += 2;
         displayForTeamA(team_a_score);
     }
 
-    public void increment1Point (View view){
+    public void increment1PointA(View view) {
         team_a_score += 1;
         displayForTeamA(team_a_score);
+    }
+
+    //    team B score
+    public void displayForTeamB(int score) {
+        TextView scoreViewB =findViewById(R.id.team_b_score);
+        scoreViewB.setText(String.valueOf(score));
+    }
+
+    public void increment3PointsB(View view) {
+        team_b_score += 3;
+        displayForTeamB(team_b_score);
+    }
+
+    public void increment2PointsB(View view) {
+        team_b_score += 2;
+        displayForTeamB(team_b_score);
+    }
+
+    public void increment1PointB(View view) {
+        team_b_score += 1;
+        displayForTeamB(team_b_score);
     }
 }
