49a5be3301d3d4ddfaa33187caddc43efeea4fb6
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-20 13:46:28 +0000

    main activity commented

--- app/src/main/java/com/example/android/basketballgame/MainActivity.java
+++ app/src/main/java/com/example/android/basketballgame/MainActivity.java
@@ -25,16 +25,19 @@ public class MainActivity extends AppCompatActivity {
         scoreViewA.setText(String.valueOf(score));
     }
 
+    //incremenet score with 3 points
     public void increment3PointsA(View view) {
         team_a_score += 3;
         displayForTeamA(team_a_score);
     }
 
+    //increment score with 2 points
     public void increment2PointsA(View view) {
         team_a_score += 2;
         displayForTeamA(team_a_score);
     }
 
+    //increment score with one point
     public void increment1PointA(View view) {
         team_a_score += 1;
         displayForTeamA(team_a_score);
@@ -46,21 +49,25 @@ public class MainActivity extends AppCompatActivity {
         scoreViewB.setText(String.valueOf(score));
     }
 
+    //increment score with 3 points
     public void increment3PointsB(View view) {
         team_b_score += 3;
         displayForTeamB(team_b_score);
     }
 
+    //increment score with 2 points
     public void increment2PointsB(View view) {
         team_b_score += 2;
         displayForTeamB(team_b_score);
     }
 
+    //increment score with 1 point
     public void increment1PointB(View view) {
         team_b_score += 1;
         displayForTeamB(team_b_score);
     }
 
+    //reset score for both teams
     public void resetScore(View view) {
         team_a_score = 0;
         team_b_score = 0;
